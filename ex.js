cbb08abc1fe117a9cf5ecda6dccbedba110f6087
Sidnei Roberto Quintal <squintal73>
2020-09-25 17:53:13 -0300

    middlewares

---
+++ ex.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const server = express()
+
+server.get('/', function(req,res){
+	res.send('<h1>Index</h1>')
+})
+server.all('/teste', function(req,res){
+        res.send('<h1>TESTE!</h1>')
+})
+
+server.get('/api/', function(req,res){
+        res.send('<h1>API!</h1>')
+})
+
+
+
+server.listen(3000, () => console.log('Executando...'))
+
