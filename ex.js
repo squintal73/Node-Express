768769c90f3b2c3e0440a36f775657967044fbe2
Sidnei Roberto Quintal <squintal73>
2021-01-22 12:05:20 -0300

    hotfix

--- ex.js
+++ ex.js
@@ -16,3 +16,18 @@ server.get('/api/', function(req,res){
 
 server.listen(3000, () => console.log('Executando...'))
 
+
+// server.get('/', function(req,res){
+// 	res.send('<h1>Index</h1>')
+// })
+// server.all('/teste', function(req,res){
+//         res.send('<h1>TESTE!</h1>')
+// })
+//
+// server.get('/api/', function(req,res){
+//         res.send('<h1>API!</h1>')
+// })
+//
+//
+//
+// server.listen(3000, () => console.log('Executando...'))
